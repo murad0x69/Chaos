9c8318c2e5c80e00b6ab5bd37984f1ee0b66e43e
Murad Alsamadi <96236051+murad0x69@users.noreply.github.com>
2024-12-18 22:39:14 +0300

    subs.sh

---
+++ subs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Download and update domains
+./chaospy.py --download-new
+./chaospy.py --download-updated
+
+if ls | grep ".zip" &> /dev/null; then
+	unzip '*.zip' &> /dev/null
+	cat *.txt | anew  domains.txtls
+	rm *.txt
+	################################################################################## Send new domains result to notify
+	echo "Hourly scan result $(date +%F-%T)"  | notify -silent -provider telegram
+	echo "Total $(wc -l < domains.txtls) new domains found" | notify -silent -provider telegram
+
+	################################################################################## Update nuclei and nuclei-templates
+ nuclei -silent -ut
+	rm *.zip
+	else
+	echo "No new programs found" | notify -silent -provider telegram
+fi
