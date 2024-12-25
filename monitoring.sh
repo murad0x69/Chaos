bd9104aab8ed88c99b2c37abf2f1b9bdc025d21b
Murad Alsamadi <96236051+murad0x69@users.noreply.github.com>
2024-12-25 05:08:38 -0800

    monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Extend PATH with Go's binary directory
 export PATH=$PATH:$(go env GOPATH)/bin
 
 # Create a folder named with today's date
@@ -9,21 +11,23 @@ mkdir -p "$folder_name"
 python chaospy.py --download-new
 python chaospy.py --download-rewards
 
-# Check for zip files and process them
+# Process ZIP files if they exist
 if ls *.zip &> /dev/null; then
+    # Extract ZIP files into the dated folder
     unzip '*.zip' -d "$folder_name" &> /dev/null
     
-    # Combine and clean up domains
-    cat "$folder_name"/*.txt | anew newdomains.md  > "$folder_name/subs.txtls"
-    cat "$folder_name/subs.txtls" | sed 's/\*.//g' > "$folder_name/domains.txtls"
+    # Combine and clean up domain lists
+    cat "$folder_name"/*.txt | anew newdomains.md > "$folder_name"/subs.txtls
+    cat "$folder_name/subs.txtls" | sed 's/\*.//g' > "$folder_name"/domains.txtls
     
-    # Clean up temporary files
+    # Remove temporary files
     rm "$folder_name"/*.txt
     rm *.zip
-    # Save new subs
-    cat "$folder_name/domains.txtls" | anew "$folder_name/newsubs.txtls"
     
-    # Send notifications about new domains
+    # Save new subdomains
+    cat "$folder_name"/domains.txtls | anew "$folder_name"/newsubs.txtls
+    
+    # Send notification about new domains
     echo "Hourly scan result $(date +%F-%T)" | notify -silent -provider telegram
-    echo "Total $(wc -l < "$folder_name/domains.txtls") new domains found" | notify -silent -provider telegram
+    echo "Total $(wc -l < "$folder_name"/domains.txtls) new domains found" | notify -silent -provider telegram
 fi
